bc68328a72e85c408b8ffa6583e97257e3adc060
Daniel Meint <d.meint@reply.de>
2020-10-07 15:48:22 +0200

    update compose files

--- stacks/infrastructure/init.sql
+++ stacks/infrastructure/init.sql
@@ -2,4 +2,4 @@ CREATE DATABASE idam;
 
 \c idam;
 
-CREATE TABLE telemetry (timestamp TIMESTAMPTZ NOT NULL, machineid TEXT NOT NULL, sensorid TEXT, value TEXT);
+CREATE TABLE telemetry (timestamp TIMESTAMPTZ NOT NULL, machineid TEXT NOT NULL, sensorid TEXT, nodehierarchy TEXT, value TEXT);
